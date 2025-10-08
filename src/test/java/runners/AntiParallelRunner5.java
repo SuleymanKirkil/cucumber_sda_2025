c3f40cc2f5f4e2a36521d156844572d7c17726bb
Süleyman Kırkıl <ali@gel.com>
2025-10-08 13:56:28 +0300

    Add Parallel Execution Functionallity

---
+++ src/test/java/runners/AntiParallelRunner5.java
@@ -0,0 +1,24 @@
+package runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {
+                "pretty", // Generates readable console output with colored text
+                "html:target/cucumber-reports/cucumber.html", // Creates HTML report at specified path
+                "json:target/cucumber-reports/cucumber.json", // Generates JSON report for integration with other tools
+                "junit:target/cucumber-reports/cucumber.xml" // Creates JUnit XML report for CI/CD systems
+        },
+        publish = true,
+        monochrome = false,
+        features = "src/test/resources/features",
+        glue = {"stepdefinitions","utilities"},
+        tags = "@positiveSignIn1",
+        dryRun = false
+)
+public class AntiParallelRunner5 {
+}
